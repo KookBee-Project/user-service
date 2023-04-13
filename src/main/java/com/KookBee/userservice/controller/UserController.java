c135da6f7622c5fa18ae3036fdb170b231c48466
HanwheeKim <hanwhee2@gmail.com>
2023-04-13 15:33:33 +0900

    #2_230413_2 feature/User/Jwt

--- src/main/java/com/KookBee/userservice/controller/UserController.java
+++ src/main/java/com/KookBee/userservice/controller/UserController.java
@@ -5,12 +5,10 @@ import com.KookBee.userservice.domain.request.UserLoginRequest;
 import com.KookBee.userservice.domain.response.UserLoginResponse;
 import com.KookBee.userservice.domain.entity.Users;
 import com.KookBee.userservice.exception.EmailCheckException;
+import com.KookBee.userservice.security.JwtService;
 import com.KookBee.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -18,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
     public UserLoginResponse login(@RequestBody UserLoginRequest request){
@@ -27,6 +26,11 @@ public class UserController {
     @PostMapping("/signup")
     public String StudentSignUp(@RequestBody Users users) throws EmailCheckException {
         return userService.studentSignUpService(users);
+    }
+
+    @GetMapping
+    public String checkJwt(){
+        return jwtService.checkToken();
     }
 
 }
