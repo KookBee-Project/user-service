4482eb54e00d3aa988d5b6ba2f45ac41d6382e7a
HanwheeKim <84313936+KimHanWhee@users.noreply.github.com>
2023-04-24 09:46:16 +0900

    Update JwtService.java

--- src/main/java/com/KookBee/userservice/security/JwtService.java
+++ src/main/java/com/KookBee/userservice/security/JwtService.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
